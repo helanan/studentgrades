e3829d43443e28303c0e5d17e6440f3ab3317a5a
Helana <helanan@gmail.com>
2017-01-26 01:48:45 -0600

    fixed javascript logic with if else need to add in for loop

--- grades.js
+++ grades.js
@@ -1,31 +1,93 @@
-var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
-var arraylength = scores.length
-var grade_A = 0;
-var grade_B = 0;
-var grade_C = 0;
-var grade_D = 0;
-var grade_F = 0;
+//practicing statements and methods with vanilla javascript
 
-for (var i = 0; i < arraylength; i++) {
+var scores;
+var grade;
+var finalGrades;
 
-}if (scores[i] >= 91){
-	grade_A++;
+scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87]; //array of score values
 
-}else (scores[i] > 80){
-	grade_B++;
+var finalGrades = scores.sort(function(a,b) {return a-b});
+console.log("finalGrades", finalGrades);
 
-}else
-	(scores[i] > 70){
-	grade_C++;
+grade = ['A', 'B', 'C', 'D', 'F']; //declares the grade variable with an identifier
 
-}else
-	(scores[i] > 60){
-	grade_D++;
 
-}else
-	(scores[i] > 50){
-	grade_F++;
+//Display the appropriate grade based on the current score
+function checkScore(finalGrades) {
+if (finalGrades >= 60) {
+	return finalGrades = 'F';
+	console.log("grade", grade);
+
+} else if (finalGrades >= 61) {
+	grade = 'D';
+	console.log("grade", grade);
+} else if (finalGrades >= 71) {
+	grade = 'C';
+	console.log("grade", grade);
+} else if (finalGrades >= 81) {
+	grade = 'B';
+	console.log("grade", grade);
+} else if (finalGrades >= 91) {
+	grade = 'C';
+	console.log("grade", grade);
+} else {
+	grade = 'No Score Found';
 }
+
+document.write('<em>' + grade + '</em>');
+
+var quantity;
+quantity = finalGrades.length;
+var el = document.getElementById('quantity');
+el.textContent = 'Number of classes taken this semester: ' + quantity;
+
+highestGrade = Math.max(finalGrades);
+console.log ("highestGrade", highestGrade);
+var el2 = document.getElementById('highest');
+el2.textContent = 'Highest Grade: ' + highestGrade;
+
+lowestGrade = Math.min(finalGrades);
+console.log ("lowestGrade", lowestGrade);
+var el3 = document.getElementById('lowest');
+el3.textContent = 'Lowest Grade: ' + lowestGrade;
+
+
+
+
+// var arrayLength = scores.length;
+// var grade_A = 0;
+// var grade_B = 0;
+// var grade_C = 0;
+// var grade_D = 0;
+// var grade_F = 0;
+
+// for (var i = 0; i < arrayLength; i++) {
+
+// if (scores[i] >= 91) {
+// 	grade_A++;
+// 	console.log("grade_A", grade_A);
+
+// } else if (scores[i] > 80) {
+// 	grade_B++;
+// 	console.log("grade_B", grade_B);
+// }
+
+// } else if (scores[i] > 70) {
+// 	grade_C++;
+// 	console.log("grade_C", grade_C);
+// }
+
+// } else if (scores[i] > 60) {
+// 	grade_D++;
+// 	console.log("grade_D", grade_D);
+// }
+
+// } else if (scores[i] > 50) {
+// 	grade_F++;
+// 	console.log("grade_F", grade_F);
+// } else {
+// 	console.log("no score recorded");
+// }
 
 //console.log()
 
